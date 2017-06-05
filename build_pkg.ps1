12888c379197089ea159cce3cdc3081af2cbf4c7
pengzhxyz <447030292@qq.com>
2017-06-05 17:58:01 +0800

    update pkg scrip to pkg full dlls

--- build_pkg.ps1
+++ build_pkg.ps1
@@ -1,3 +1,17 @@
-﻿Compress-Archive Images, Wox.Plugin.Youdao.dll, plugin.json,Wox.Plugin.Youdao.pdb youdao-pk.zip -Force
+﻿$files = "Images","plugin.json"
+[System.Collections.ArrayList]$files = $files
+$files.Add("Newtonsoft.Json.dll")
+$files.Add("Newtonsoft.Json.xml")
+$files.Add("NLog.dll")
+$files.Add("NLog.xml")
+$files.Add("Pinyin4Net.dll")
+$files.Add("Wox.Infrastructure.dll")
+$files.Add("Wox.Infrastructure.pdb")
+$files.Add("Wox.Plugin.dll")
+$files.Add("Wox.Plugin.pdb")
+$files.Add("Wox.Plugin.Youdao.dll")
+$files.Add("Wox.Plugin.Youdao.pdb")
 
-Rename-Item youdao-pk.zip youdao-pk.wox
+Compress-Archive $files youdao-pk-full.zip -Force
+
+Rename-Item youdao-pk-full.zip youdao-pk-full.wox
